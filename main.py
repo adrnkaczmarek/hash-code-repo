f8718f72ca74d9e970841249183ca9f8ccc76a48
adrnkaczmarek <adrn.kaczmarek@gmail.com>
2018-03-01 19:20:39 +0100

    numpy added

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import math
+import numpy as np
 
 
 def distance(a, b, x, y):
-    return math.fabs(a - x) + math.fabs(b - y)
+    return int(math.fabs(a - x) + math.fabs(b - y))
+
+
+example = np.genfromtxt('a_example.in', dtype='int')
+should_be_easy = np.genfromtxt('b_should_be_easy.in')
+no_hurry = np.genfromtxt('c_no_hurry.in')
 
 
 print(distance(1, 2, 4, 7))
+print(example)
