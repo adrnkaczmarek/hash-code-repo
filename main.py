08f40d5bcdd46c4bcae6eea6e8a77aef7cd807e3
adrnkaczmarek <adrn.kaczmarek@gmail.com>
2018-03-01 21:33:39 +0100

    alll work!!!

--- main.py
+++ main.py
@@ -7,24 +7,22 @@ def distance(a, b, x, y):
 
 
 def time_to_start_ride(car_x, car_y, ride_x, ride_y, current_time, start_time):
-    turns = 0
-
-    if current_time > start_time:
-        turns = start_time - current_time
-    else:
-        dist = distance(car_x, car_y, ride_x, ride_y)
-        turns = current_time + dist - start_time
-
-    return turns
+    dist = distance(car_x, car_y, ride_x, ride_y)
+    return (start_time - current_time) - dist
 
 
-def calculate_time(vehicle, ride, step):
-    vehicle_x = current_vehicle_positions[vehicle, 0]
-    vehicle_y = current_vehicle_positions[vehicle, 1]
-    ride_x = rides_data[ride, 0]
-    ride_y = rides_data[ride, 1]
-    start = rides_data[ride, 4]
-    return time_to_start_ride(vehicle_x, vehicle_y, ride_x, ride_y, step, start)
+def calculate_wait_time(vehicle_position, ride_info, step):
+    vehicle_x = vehicle_position[0]
+    vehicle_y = vehicle_position[1]
+    ride_x = ride_info[0]
+    ride_y = ride_info[1]
+    start = ride_info[4]
+    calculated = time_to_start_ride(vehicle_x, vehicle_y, ride_x, ride_y, step, start)
+    return calculated
+
+
+def calculate_trip_time(ride_info):
+    return distance(ride_info[0], ride_info[1], ride_info[2], ride_info[3])
 
 
 if __name__ == '__main__':
@@ -32,22 +30,44 @@ if __name__ == '__main__':
     should_be_easy = np.genfromtxt('b_should_be_easy.in')
     no_hurry = np.genfromtxt('c_no_hurry.in')
 
-    output = []
     general_data = example[0]
     rides_data = example[1:]
     vehicles = general_data[2]
     rides = general_data[3]
     steps = general_data[5]
-    current_vehicle_positions = np.zeros((vehicles, 2))
+    vehicle_info = np.zeros((vehicles, 3))
+    output = []
+
+    for i in range(vehicles):
+        output.append([0])
+
+    rides_with_flag = np.zeros((rides, 7))
+    rides_with_flag[:, :-1] = rides_data
+    rides_data = rides_with_flag
 
     for step in range(steps):
         for vehicle in range(vehicles):
-            min_ride = calculate_time(vehicle, 0, step)
-            min_ride_index = 0
 
-            for ride in range(1, rides):
-                current_min_ride = calculate_time(vehicle, ride, step)
+            if vehicle_info[vehicle, 2] == step:
+                min_ride = 1000000000000
+                min_ride_index = -1
 
-                if current_min_ride < min_ride:
-                    min_ride = current_min_ride
-                    min_ride_index = ride
+                for ride in range(0, rides):
+                    if rides_data[ride, 6] == 0:
+                        current_min_ride = calculate_wait_time(vehicle_info[vehicle], rides_data[ride], step)
+
+                        if current_min_ride >= 0:
+                            if current_min_ride < min_ride:
+                                min_ride = current_min_ride
+                                min_ride_index = ride
+
+                if min_ride_index >= 0:
+                    output[vehicle].append(min_ride_index)
+                    # vehicle_row = np.append(output[vehicle], min_ride_index)
+                    # output[vehicle] = vehicle_row
+                    tmp = output[vehicle][0]
+                    output[vehicle][0] = output[vehicle][0] + 1
+                    vehicle_info[vehicle, 2] = min_ride + step + calculate_trip_time(rides_data[min_ride_index])
+                    rides_data[min_ride_index, 6] = 1
+
+    print(output)
