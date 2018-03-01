d6584c3dc17131f48bd977bef04895988fee5fae
adrnkaczmarek <adrn.kaczmarek@gmail.com>
2018-03-01 20:06:58 +0100

    find minimum ride

--- main.py
+++ main.py
@@ -6,10 +6,46 @@ def distance(a, b, x, y):
     return int(math.fabs(a - x) + math.fabs(b - y))
 
 
-example = np.genfromtxt('a_example.in', dtype='int')
-should_be_easy = np.genfromtxt('b_should_be_easy.in')
-no_hurry = np.genfromtxt('c_no_hurry.in')
+def time_to_start_ride(car_x, car_y, ride_x, ride_y, current_time, start_time):
+    turns = 0
+
+    if current_time > start_time:
+        turns = start_time - current_time
+    else:
+        dist = distance(car_x, car_y, ride_x, ride_y)
+        turns = current_time + dist - start_time
+
+    return turns
 
 
-print(distance(1, 2, 4, 7))
-print(example)
+def calculate_time(vehicle, ride, step):
+    vehicle_x = current_vehicle_positions[vehicle, 0]
+    vehicle_y = current_vehicle_positions[vehicle, 1]
+    ride_x = rides_data[ride, 0]
+    ride_y = rides_data[ride, 1]
+    start = rides_data[ride, 4]
+    return time_to_start_ride(vehicle_x, vehicle_y, ride_x, ride_y, step, start)
+
+
+if __name__ == '__main__':
+    example = np.genfromtxt('a_example.in', dtype='int')
+    should_be_easy = np.genfromtxt('b_should_be_easy.in')
+    no_hurry = np.genfromtxt('c_no_hurry.in')
+
+    output = []
+    general_data = example[0]
+    rides_data = example[1:]
+    vehicles = general_data[2]
+    rides = general_data[3]
+    steps = general_data[5]
+    current_vehicle_positions = np.zeros(vehicles, 2)
+
+    for step in range(steps):
+        for vehicle in range(vehicles):
+            min_ride = calculate_time(vehicle, 0, step)
+
+            for ride in range(1, rides):
+                min_ride = calculate_time(vehicle, ride, step)
+
+    print(distance(1, 2, 4, 7))
+    print(example)
